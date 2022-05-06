7cd506b4f55f5d0d04932908e30742e5cb038fac
Teoman Akgün <teomanakgn84@gmail.com>
2022-05-07 01:55:28 +0300

    odev

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Yusei';
+    : 'Dark';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Fudo';
+    : 'Souls';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
