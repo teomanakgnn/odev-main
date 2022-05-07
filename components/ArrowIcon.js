568b5a114fa6f526d8addaa1e51a390741cdb204
Teoman Akgün <teomanakgn84@gmail.com>
2022-05-07 21:04:46 +0300

    odev

--- components/ArrowIcon.js
+++ components/ArrowIcon.js
@@ -2,8 +2,8 @@ export default function ArrowIcon({ className, color = 'text-primary' }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="0"
+      height="0"
       fill="none"
       viewBox="0 0 24 24"
       className={className}
